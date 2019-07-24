89e6f94dfe22363297624a4d9e3ca72ec6b2f9b9
saurabh dasgupta <saurabh_dasgupta@hotmail.com>
2019-07-24 12:41:00 +0100

    Added Demo.ConsoleEXECore

---
+++ Demo.ConsoleEXECore/ConsoleTraceListener.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace System.Diagnostics
+{
+    public class ConsoleTraceListener : TraceListener
+    {
+        public override void Write(string message)
+        {
+            Console.Write(message);
+        }
+
+        public override void WriteLine(string message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+}
